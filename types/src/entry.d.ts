74eaf3a5bfd51c051a636e85592d5d9d9dcd3412
aki <lala.akira@gmail.com>
2020-05-03 08:10:56 +0900

    refactor: Remove unused files

--- types/src/entry.d.ts
+++
@@ -1,4 +0,0 @@
-import component from "./BootstrapToast.vue";
-import { open, showSuccess, showError, showErrors } from "./programmatic";
-export default component;
-export { open, showSuccess, showError, showErrors };
